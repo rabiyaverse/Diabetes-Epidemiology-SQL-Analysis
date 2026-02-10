23debcf6eb39f9df0e1a00b69132c1eac317ead9
Rabiyatul Adawiyah <rabiyatuladawiyahnasution@gmail.com>
2026-02-10 13:23:47 +1100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -9,3 +9,14 @@ SELECT
   COUNT(*) AS n
 FROM diabetes_dataset
 GROUP BY outcome;
+
+-- Mean BMI and sample size by diabetes outcome
+SELECT
+    outcome,
+    COUNT(*) AS n_participants,
+    AVG(TRY_CAST(bmi AS FLOAT)) AS mean_bmi
+FROM dbo.diabetes_dataset
+WHERE TRY_CAST(bmi AS FLOAT) IS NOT NULL
+GROUP BY outcome;
+
+-- 
